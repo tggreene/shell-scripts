df50e5c9fc390f267b4e4bcd926aa6bf79851cab
Tim Greene <tim.g.greene@gmail.com>
2020-05-13 11:18:02 +0100

    Initial commit

---
+++ src/inspect-jwt.clj
@@ -0,0 +1,26 @@
+#!/usr/bin/env bb
+(ns inspect-jwt
+  "Decodes base64 header and payload of a jwt, does not have any interest in verification"
+  (:require [clojure.string :as str]))
+
+(def decoder (java.util.Base64/getDecoder))
+
+(defn base64->string
+  [in]
+  (String. (.decode decoder in)))
+
+(defn -main
+  [& args]
+  (let [jwt (first args)]
+    (assert (and (= (count args) 1)
+                 (string? jwt)
+                 (re-matches #".+\..+\..+" jwt))
+            "Must have only 1 argument of a syntactically correct JWT token")
+    (let [[header payload]
+          (map base64->string (take 2 (str/split jwt #"\.")))]
+      (printf "Header: %s\nPayload: %s\n" header payload))))
+
+(try
+  (apply -main *command-line-args*)
+  (catch Exception e
+    (println e)))
